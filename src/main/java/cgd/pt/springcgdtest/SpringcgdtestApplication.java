656bf0cd892d83a6f24c09514e786a79f39abf9c
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-17 20:20:28 +0000

    Initial commit

---
+++ src/main/java/cgd/pt/springcgdtest/SpringcgdtestApplication.java
@@ -0,0 +1,13 @@
+package cgd.pt.springcgdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcgdtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcgdtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cgd/pt/springcgdtest/SpringcgdtestApplicationTests.java
@@ -0,0 +1,13 @@
+package cgd.pt.springcgdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcgdtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

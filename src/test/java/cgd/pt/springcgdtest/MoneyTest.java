061c662e0892ecdfeb09ccf6977205cab73b6bb2
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-17 23:16:01 +0000

    Classe de Teste criada, relates #2

---
+++ src/test/java/cgd/pt/springcgdtest/MoneyTest.java
@@ -0,0 +1,19 @@
+package cgd.pt.springcgdtest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTest {
+
+    @Test
+    void multiplicationTest() {
+
+        Euro cinco = new Euro(5);
+
+        cinco.times(2);
+
+        assertEquals(10, cinco.amount);
+
+    }
+}

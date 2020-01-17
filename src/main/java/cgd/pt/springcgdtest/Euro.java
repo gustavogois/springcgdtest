552e4645d88c7aa9d481b3c52cd62913e474183b
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-17 23:24:26 +0000

    Teste passou, relates #2

---
+++ src/main/java/cgd/pt/springcgdtest/Euro.java
@@ -0,0 +1,14 @@
+package cgd.pt.springcgdtest;
+
+public class Euro {
+
+    public int amount;
+
+    public Euro(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        this.amount = this.amount * multiplier;
+    }
+}

6e9234d8d9ae23152a6ad55aadd1c671f7a64d65
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-18 10:25:53 +0000

    Refactoring - relates #10

--- src/main/java/cgd/pt/springcgdtest/Euro.java
+++ src/main/java/cgd/pt/springcgdtest/Euro.java
@@ -2,13 +2,16 @@ package cgd.pt.springcgdtest;
 
 public class Euro extends Money {
 
+    private String currency;
+
     public Euro(int amount) {
         this.amount = amount;
+        this.currency = "€";
     }
 
     @Override
     public String currency() {
-        return "€";
+        return currency;
     }
 
     public Money times(int multiplier) {
--- src/main/java/cgd/pt/springcgdtest/Real.java
+++ src/main/java/cgd/pt/springcgdtest/Real.java
@@ -2,13 +2,17 @@ package cgd.pt.springcgdtest;
 
 public class Real extends Money {
 
+    private String currency;
+
     public Real(int amount) {
         this.amount = amount;
+        this.currency = "R$";
     }
 
     @Override
     public String currency() {
-        return "R$";
+        currency = "R$";
+        return currency;
     }
 
     public Money times(int multiplier) {

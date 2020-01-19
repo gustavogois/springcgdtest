e8090e6fbeed1fe80e665a84e07a47f76001ea76
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-19 09:17:32 +0000

    Passo 3 - Utilização de Pair em Bank relates #12

--- src/main/java/cgd/pt/springcgdtest/Bank.java
+++ src/main/java/cgd/pt/springcgdtest/Bank.java
@@ -11,6 +11,10 @@ public class Bank {
     }
 
     public int rate(String from, String to) {
+        if (from.equals(to)) {
+            return 1;
+        }
+
         return rateMap.get(new Pair(from, to));
     }
 

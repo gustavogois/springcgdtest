e301dfc47a7b628049206831f5e0873c69075fb6
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-19 09:08:56 +0000

    Passo 3 - Utilização de Pair em Bank relates #12

--- src/main/java/cgd/pt/springcgdtest/Bank.java
+++ src/main/java/cgd/pt/springcgdtest/Bank.java
@@ -1,6 +1,10 @@
 package cgd.pt.springcgdtest;
 
+import java.util.HashMap;
+
 public class Bank {
+
+    private HashMap<Pair, Integer> rateMap = new HashMap<>();
 
     public Money reduce(Expression expression, String toCurrency) {
         return expression.reduce(this, toCurrency);
@@ -11,6 +15,6 @@ public class Bank {
     }
 
     public void addRate(String from, String to, int rate) {
-
+        rateMap.put(new Pair(from, to), rate);
     }
 }

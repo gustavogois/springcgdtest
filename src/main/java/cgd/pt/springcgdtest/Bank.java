d0af48d594709c4b25155c2b1677468523ee6483
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-19 08:52:55 +0000

    Passo 3 - Bank.rate implementation relates #12

--- src/main/java/cgd/pt/springcgdtest/Bank.java
+++ src/main/java/cgd/pt/springcgdtest/Bank.java
@@ -1,9 +1,12 @@
 package cgd.pt.springcgdtest;
 
 public class Bank {
+
     public Money reduce(Expression expression, String toCurrency) {
-
         return expression.reduce(toCurrency);
+    }
 
+    public int rate(String from, String to) {
+        return (from.equals("R$") && to.equals("€")) ? 5 : 1;
     }
 }

339340785bbfd92ca03b2ebd54f641bbb8f353c1
Gustavo Gois <gustavo.gois@gmail.com>
2020-01-19 00:26:07 +0000

    closes #9

--- src/main/java/cgd/pt/springcgdtest/Euro.java
+++
@@ -1,9 +0,0 @@
-package cgd.pt.springcgdtest;
-
-public class Euro extends Money {
-
-    public Euro(int amount, String currency) {
-        super(amount, currency);
-    }
-
-}
--- src/main/java/cgd/pt/springcgdtest/Real.java
+++
@@ -1,9 +0,0 @@
-package cgd.pt.springcgdtest;
-
-public class Real extends Money {
-
-    public Real(int amount, String currency) {
-        super(amount, currency);
-    }
-
-}
